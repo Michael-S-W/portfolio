2dfdc673f69e6c200612f664b3c5e846e2f64317
MSW <Michael_s.wilson@ymail.com>
2024-03-13 18:58:22 +0200

    updating transform by js

---
+++ script.js
@@ -0,0 +1,13 @@
+let pages = document.querySelector(".pages");
+let inputs = document.querySelectorAll(".portfolio");
+
+// console.log(inputs);
+
+inputs.forEach((e, i) => {
+  //   console.log(e, i);
+  e.addEventListener("change", () => {
+    let y = i * 20;
+    pages.style.transform = `translateX(-${y}%)`;
+  });
+  //   e.checked ? (pages.style.transform = `translateX(${i * -20}%)`) : "";
+});
