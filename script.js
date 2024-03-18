2b6176c51d9d5f484cc96a1412b1c611aed430a9
MSW <Michael_s.wilson@ymail.com>
2024-03-18 15:57:28 +0200

    finalizing code

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 let pages = document.querySelector(".pages");
 let inputs = document.querySelectorAll(".portfolio");
 
-// console.log(inputs);
-
 inputs.forEach((e, i) => {
-  //   console.log(e, i);
   e.addEventListener("change", () => {
     let y = i * (100 / 3);
     pages.style.transform = `translateX(-${y}%)`;
   });
-  //   e.checked ? (pages.style.transform = `translateX(${i * -20}%)`) : "";
 });
