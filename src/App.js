42628e8ab9e7594e0b193bc077178a8fe00011b2
Michael <michael_s.wilson@ymail.com>
2024-12-17 18:59:01 +0200

    added images and icons 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import { useState } from 'react';
 
 function App() {
+  const [theme, setTheme] = useState(true)
   return (
-    <div className="App">
-      <Navbar/>
+    <div className={theme?"App dark":"App"}>
+      <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,30 @@
 import './Navbar.css'
-
 import React from 'react'
+import logo from '../icons-svg/logo-new.png'
+// import homeLogo from '../icons-svg/home.svg'
+// import projectsLogo from '../icons-svg/projects.svg'
+// import contactLogo from '../icons-svg/call.svg'
 
-export default function Navbar() {
+
+
+export default function Navbar({theme, setTheme}) {
+
+
   return (
     <div className='navbar'>
+      <div className='navbar-logo'>
+        <img src={logo} alt="logo"/>
+        <span>
+        Portfolio
+        </span>
+      </div>
+      <div className='navbar-list'>
+        <span className='navbar-list-item'>Home</span>
+        <span className='navbar-list-item'>Projects</span>
+        <span className='navbar-list-item'>Contact</span>
+        <button onClick={()=>setTheme(p=>!p)}>{theme?<span>🌞</span>:<span>🌚</span>}</button>
+        
+      </div>
       
     </div>
   )
