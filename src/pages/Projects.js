1c391881d56fae9cf51cfc7692ec6434932a8d71
Michael <michael_s.wilson@ymail.com>
2025-02-19 22:21:09 +0200

    final touches 2

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -31,34 +31,33 @@ function Projects() {
 
   return (
     <div className='projects'>
-      <h1 style={theme?{color:"white"}:{color:"black"}}>Projects</h1>
       <div className='project' >
-      <RotatingLines
-        visible={!github}
-        height="96"
-        width="96"
-        color="blue"
-        strokeWidth="5"
-        animationDuration="0.75"
-        ariaLabel="rotating-lines-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-      />
-        {github&&
-        github.map((data)=>
-            <div key={data.id} >
-              <div className='project-text'>
-              <button 
-              id={data.name} 
-              onClick={handleClick}
-              style={theme?{color:"white"}:{color:"black"}}
-              >{data.name}</button>
-              <a href={data.html_url} target='_blank' rel='noreferrer'>Source code here</a>
-              {/* <hr/> */}
+        <RotatingLines
+          visible={!github}
+          height="96"
+          width="96"
+          color="blue"
+          strokeWidth="5"
+          animationDuration="0.75"
+          ariaLabel="rotating-lines-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+          {github&&
+          github.map((data)=>
+              <div key={data.id} >
+                <div className='project-text'>
+                <button 
+                id={data.name} 
+                onClick={handleClick}
+                style={theme?{color:"white"}:{color:"black"}}
+                >{data.name}</button>
+                <a href={data.html_url} target='_blank' rel='noreferrer'>Source code here</a>
+                {/* <hr/> */}
+                </div>
               </div>
-            </div>
-        )
-      }
+          )
+        }
       </div>
         {liveLink?<iframe className='project-iframe' src={liveLink} title='liveMirror' width={'100%'} height={'100%'} style={{margin:"0 0"}}></iframe>:<h2>Click project to view</h2>}
     </div>
